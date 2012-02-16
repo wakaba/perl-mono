3e0c76d91678288c1b6cedabd88b0c444f00f82e
wakabatan <wakabatan@localhost.localdomain>
2012-02-16 14:03:16 +0900

    Added to the repo

---
+++ lib/Test/Mono.pm
@@ -0,0 +1,69 @@
+package Test::Mono;
+use strict;
+use warnings;
+our $VERSION = '1.0';
+use Exporter::Lite;
+use Mono::ID;
+
+our @EXPORT = qw(
+    create_asin
+    create_isbn10
+    create_isbn13
+    create_ean13
+
+    create_broken_isbn10
+    create_broken_isbn13
+);
+
+my @ASIN_ALPHABET = ('0'..'9', 'A'..'Z');
+
+sub create_asin (;%) {
+    my (%args) = @_;
+    {
+        my $v = '';
+        $v .= $ASIN_ALPHABET[rand @ASIN_ALPHABET] for 1..10;
+        if (is_asin $v) {
+            return $v;
+        }
+        redo;
+    }
+}
+
+my @ISBN_ALPHABET = ('0'..'9');
+
+sub create_isbn10 (;%) {
+    my $v = '';
+    $v .= $ISBN_ALPHABET[rand @ISBN_ALPHABET] for 1..9;
+    $v .= calculate_isbn10_check_digit $v . '?';
+    return $v;
+}
+
+sub create_broken_isbn10 (;%) {
+    my $v = create_isbn10;
+    $v++;
+    $v =~ tr/Y/0/;
+    return sprintf '%010s', $v;
+}
+
+sub create_isbn13 (;%) {
+    my $ean = '978' . create_isbn10;
+    return substr($ean, 0, 12) . calculate_ean_check_digit $ean;
+}
+
+sub create_broken_isbn13 (;%) {
+    my $v = create_isbn13;
+    $v++;
+    return sprintf '%013d', $v;
+}
+
+sub create_ean13 (;%) {
+    {
+        my $v = '';
+        $v .= $ISBN_ALPHABET[rand @ISBN_ALPHABET] for 1..12;
+        $v .= calculate_ean_check_digit $v . '?';
+        return $v unless is_isbn13 $v;
+        redo;
+    }
+}
+
+1;
---
+++ t/test/mono.t
@@ -0,0 +1,60 @@
+package test::Test::Mono;
+use strict;
+use warnings;
+use Path::Class;
+use lib file(__FILE__)->dir->parent->parent->subdir('lib')->stringify;
+use lib file(__FILE__)->dir->parent->parent->subdir('t', 'lib')->stringify;
+use lib file(__FILE__)->dir->parent->parent->subdir('modules', 'perl-test-moremore', 'lib')->stringify;
+use base qw(Test::Class);
+use Test::Mono;
+use Test::MoreMore;
+use Mono::ID;
+
+sub _create_asin : Test(3) {
+    for (1..3) {
+        my $asin = create_asin;
+        ok is_asin $asin;
+    }
+}
+
+sub _create_isbn10 : Test(3) {
+    for (1..3) {
+        my $isbn = create_isbn10;
+        ok is_isbn10 $isbn;
+    }
+}
+
+sub _create_broken_isbn10 : Test(6) {
+    for (1..3) {
+        my $isbn = create_broken_isbn10;
+        ng is_isbn10 $isbn;
+        is length $isbn, 10;
+    }
+}
+
+sub _create_isbn13 : Test(3) {
+    for (1..3) {
+        my $isbn = create_isbn13;
+        ok is_isbn13 $isbn;
+    }
+}
+
+sub _create_broken_isbn13 : Test(6) {
+    for (1..3) {
+        my $isbn = create_broken_isbn13;
+        ng is_isbn13 $isbn;
+        is length $isbn, 13;
+    }
+}
+
+sub _create_ean13 : Test(6) {
+    for (1..3) {
+        my $ean = create_ean13;
+        ok is_ean $ean;
+        ng is_isbn13 $ean;
+    }
+}
+
+__PACKAGE__->runtests;
+
+1;
